4f6e742619e754420aa05b51931eac52f838dc27
niknik24 <64371527+niknik24@users.noreply.github.com>
2022-11-13 11:49:36 +0300

    Added comments

--- Assets/Scripts/Bonuses/BonusBase.cs
+++ Assets/Scripts/Bonuses/BonusBase.cs
@@ -16,17 +16,17 @@ public class BonusBase : MonoBehaviour
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
-        rend.color = bonColor;
+        rend.color = bonColor; // Присваиваем префабу нужный цвет
         if (textObject != null)
         {
-            textComponent = textObject.GetComponent<Text>();
+            textComponent = textObject.GetComponent<Text>(); // Присваиваем префабу нужный текст и цвет текста
             textComponent.color = textColor;
             textComponent.text = text;
         }
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // Добавляем триггеры. Если коснулся "игрока", то добавить бонус, потом уничтожить. Если коснулся нижней стенки, то просто уничтожить
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -39,7 +39,7 @@ public class BonusBase : MonoBehaviour
         }
     }
 
-    public virtual void BonusActivate()
+    public virtual void BonusActivate() // Добавляем 100 очков к общему кол-ву
     {
         gameData.points += 100;
     }
